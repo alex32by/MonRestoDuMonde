946a312c8454b690dff29f0a2de1c0cf31258ceb
Alex Bury <alex@Air-de-Alex.PIERREB>
2018-08-07 19:17:30 +0200

    Ajout des controlleurs principaux

---
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Alex Bury on 7/08/18.
+//  Copyright © 2018 Alex BURY. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
