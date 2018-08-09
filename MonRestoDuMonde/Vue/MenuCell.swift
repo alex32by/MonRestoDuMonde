a8fb24d7937001fee936abb0a93f865d39b706e6
Alex Bury <alex@Air-de-Alex.PIERREB>
2018-08-09 12:48:09 +0200

    Ajout Collection View

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Alex Bury on 9/08/18.
+//  Copyright © 2018 Alex BURY. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
