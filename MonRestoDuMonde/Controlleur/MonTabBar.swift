c9ab34872043c45f597642756d0eddd133a7caf3
Alex Bury <alex@Air-de-Alex.PIERREB>
2018-08-09 12:22:17 +0200

    TabBarCustom

---
+++ MonRestoDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Alex Bury on 7/08/18.
+//  Copyright © 2018 Alex BURY. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constante.swift
@@ -0,0 +1,14 @@
+//
+//  Constante.swift
+//  MonRestoDuMonde
+//
+//  Created by Alex Bury on 7/08/18.
+//  Copyright © 2018 Alex BURY. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+
